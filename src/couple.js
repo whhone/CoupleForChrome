325c4c84afcb2f6f33decb7894ff60270723aba0
Wai Hon Law <whhone@whhone-mbp.local>
2015-04-28 17:13:24 -0700

    first commit

---
+++ src/couple.js
@@ -0,0 +1,110 @@
+window.onresize = doLayout;
+var isLoading = false;
+
+onload = function() {
+  var webview = document.querySelector('webview');
+  doLayout();
+
+  var version = navigator.appVersion.substr(navigator.appVersion.lastIndexOf('Chrome/') + 7);
+  var match = /([0-9]*)\.([0-9]*)\.([0-9]*)\.([0-9]*)/.exec(version);
+  var majorVersion = parseInt(match[1]);
+  var buildVersion = parseInt(match[3]);
+
+  document.querySelector('#reload').addEventListener(
+    'webkitAnimationIteration',
+    function() {
+      if (!isLoading) {
+        document.body.classList.remove('loading');
+      }
+    });
+
+  webview.addEventListener('exit', handleExit);
+  webview.addEventListener('loadstart', handleLoadStart);
+  webview.addEventListener('loadstop', handleLoadStop);
+  webview.addEventListener('loadabort', handleLoadAbort);
+  webview.addEventListener('loadredirect', handleLoadRedirect);
+  webview.addEventListener('loadcommit', handleLoadCommit);
+};
+
+function doLayout() {
+  var webview = document.querySelector('webview');
+  var controls = document.querySelector('#controls');
+  var controlsHeight = controls.offsetHeight;
+  var windowWidth = document.documentElement.clientWidth;
+  var windowHeight = document.documentElement.clientHeight;
+  var webviewWidth = windowWidth;
+  var webviewHeight = windowHeight - controlsHeight;
+
+  webview.style.width = webviewWidth + 'px';
+  webview.style.height = webviewHeight + 'px';
+
+  var sadWebview = document.querySelector('#sad-webview');
+  sadWebview.style.width = webviewWidth + 'px';
+  sadWebview.style.height = webviewHeight * 2/3 + 'px';
+  sadWebview.style.paddingTop = webviewHeight/3 + 'px';
+}
+
+function handleExit(event) {
+  console.log(event.type);
+  document.body.classList.add('exited');
+  if (event.type == 'abnormal') {
+    document.body.classList.add('crashed');
+  } else if (event.type == 'killed') {
+    document.body.classList.add('killed');
+  }
+}
+
+function resetExitedState() {
+  document.body.classList.remove('exited');
+  document.body.classList.remove('crashed');
+  document.body.classList.remove('killed');
+}
+
+function handleLoadCommit(event) {
+  resetExitedState();
+  if (!event.isTopLevel) {
+    return;
+  }
+
+  document.querySelector('#location').value = event.url;
+
+  var webview = document.querySelector('webview');
+  document.querySelector('#back').disabled = !webview.canGoBack();
+  document.querySelector('#forward').disabled = !webview.canGoForward();
+  closeBoxes();
+}
+
+function handleLoadStart(event) {
+  document.body.classList.add('loading');
+  isLoading = true;
+
+  resetExitedState();
+  if (!event.isTopLevel) {
+    return;
+  }
+
+  document.querySelector('#location').value = event.url;
+}
+
+function handleLoadStop(event) {
+  // We don't remove the loading class immediately, instead we let the animation
+  // finish, so that the spinner doesn't jerkily reset back to the 0 position.
+  isLoading = false;
+}
+
+function handleLoadAbort(event) {
+  console.log('LoadAbort');
+  console.log('  url: ' + event.url);
+  console.log('  isTopLevel: ' + event.isTopLevel);
+  console.log('  type: ' + event.type);
+}
+
+function handleLoadRedirect(event) {
+  resetExitedState();
+  if (!event.isTopLevel) {
+    return;
+  }
+
+  document.querySelector('#location').value = event.newUrl;
+}
+
