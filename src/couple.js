c791684f84e1502f9dd858d4117abb134f04081f
Wai Hon Law <whhone@whhone-mbp.local>
2015-04-29 22:35:55 -0700

    more cleanup

--- src/couple.js
+++ src/couple.js
@@ -3,14 +3,7 @@ var isLoading = false;
 
 onload = function() {
   doLayout();
-
-  var webview = document.querySelector('webview');
-  webview.addEventListener('exit', handleExit);
-  webview.addEventListener('loadstart', handleLoadStart);
-  webview.addEventListener('loadstop', handleLoadStop);
-  webview.addEventListener('loadabort', handleLoadAbort);
-  webview.addEventListener('loadredirect', handleLoadRedirect);
-  webview.addEventListener('loadcommit', handleLoadCommit);
+  // var webview = document.querySelector('webview');
 };
 
 function doLayout() {
@@ -28,68 +21,3 @@ function doLayout() {
   sadWebview.style.height = webviewHeight * 2/3 + 'px';
   sadWebview.style.paddingTop = webviewHeight/3 + 'px';
 }
-
-function handleExit(event) {
-  console.log(event.type);
-  document.body.classList.add('exited');
-  if (event.type == 'abnormal') {
-    document.body.classList.add('crashed');
-  } else if (event.type == 'killed') {
-    document.body.classList.add('killed');
-  }
-}
-
-function resetExitedState() {
-  document.body.classList.remove('exited');
-  document.body.classList.remove('crashed');
-  document.body.classList.remove('killed');
-}
-
-function handleLoadCommit(event) {
-  resetExitedState();
-  if (!event.isTopLevel) {
-    return;
-  }
-
-  document.querySelector('#location').value = event.url;
-
-  var webview = document.querySelector('webview');
-  document.querySelector('#back').disabled = !webview.canGoBack();
-  document.querySelector('#forward').disabled = !webview.canGoForward();
-  closeBoxes();
-}
-
-function handleLoadStart(event) {
-  document.body.classList.add('loading');
-  isLoading = true;
-
-  resetExitedState();
-  if (!event.isTopLevel) {
-    return;
-  }
-
-  document.querySelector('#location').value = event.url;
-}
-
-function handleLoadStop(event) {
-  // We don't remove the loading class immediately, instead we let the animation
-  // finish, so that the spinner doesn't jerkily reset back to the 0 position.
-  isLoading = false;
-}
-
-function handleLoadAbort(event) {
-  console.log('LoadAbort');
-  console.log('  url: ' + event.url);
-  console.log('  isTopLevel: ' + event.isTopLevel);
-  console.log('  type: ' + event.type);
-}
-
-function handleLoadRedirect(event) {
-  resetExitedState();
-  if (!event.isTopLevel) {
-    return;
-  }
-
-  document.querySelector('#location').value = event.newUrl;
-}
-
